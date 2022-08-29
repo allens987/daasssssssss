d9fbc4039ed3a952183cae485507797c3ae9ffec
YTiceice <a104321212@gmail.com>
2022-08-29 19:08:26 +0800

    feat: first push

---
+++ src/main/java/Diamonds/vdr/DiamondsPlugin/Main.java
@@ -0,0 +1,23 @@
+package Diamonds.vdr.DiamondsPlugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import Diamonds.vdr.DiamondsPlugin.Commands.diamond;
+
+public class Main extends JavaPlugin{
+
+public static Main Diamond;
+
+    @Override
+    public void onEnable() {
+
+       Diamond = this;
+
+       getLogger().info("����Ұʧ���...");
+       Bukkit.getPluginCommand("diamond").setExecutor(new diamond());
+    }
+    public void onDisable() {
+        getLogger().info("������������...");    
+    }
+}
