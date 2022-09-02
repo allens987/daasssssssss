664458c1bc2ab22b950f7cbecb601f5788ff4168
wuize <102895127+wuize@users.noreply.github.com>
2022-09-02 19:25:32 +0800

    Update sc.java

--- src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/sc.java
+++ src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/sc.java
@@ -16,5 +16,21 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class sc implements CommandExecutor {
+    public class PlayerJoinListener implements Listener {
+        @EventHandler
+        public void onPlayerJoin(PlayerJoinEvent evt) {
+            Player joined = evt.getPlayer();
+            String joinedName = joined.getName();
+
+            //RETRIEVING THE JOIN MESSAGE ALREADY SET
+            String joinMessage = evt.getJoinMessage();
+
+            //SETTING THE JOIN MESSAGE
+            evt.setJoinMessage(joinedName + " has joined the game");
+
+            //CLEARING THE JOIN MESSAGE
+            evt.setJoinMessage(null);
+        }
+    }
 
     
