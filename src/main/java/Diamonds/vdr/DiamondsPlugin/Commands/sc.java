8b998554a4eba989cd679a01423075b992c69b19
wuize <102895127+wuize@users.noreply.github.com>
2022-09-02 19:24:00 +0800

    Create sc.java

---
+++ src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/sc.java
@@ -0,0 +1,20 @@
+package Diamonds.vdr.DiamondsPlugin.Commands;
+
+import java.util.ArrayList;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class sc implements CommandExecutor {
+
+    
