7da551e667640fa00ff967272205411e03694c6e
wuize <102895127+wuize@users.noreply.github.com>
2022-09-03 13:17:10 +0800

    Update sc.java

--- src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/sc.java
+++ src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/sc.java
@@ -1,27 +1,33 @@
 package Diamonds.vdr.DiamondsPlugin.Commands;
+
 import java.util.ArrayList;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-    public class sc implements Listener {
-        @EventHandler
-        public void onPlayerJoin(PlayerJoinEvent evt) {
-            Player joined = evt.getPlayer();
-            String joinedName = joined.getName();
 
-            //RETRIEVING THE JOIN MESSAGE ALREADY SET
-            String joinMessage = evt.getJoinMessage();
+public class sc implements Listener {
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent evt) {
+        Player joined = evt.getPlayer();
+        String joinedName = joined.getName();
 
-            //SETTING THE JOIN MESSAGE
-            evt.setJoinMessage(joinedName + " has joined the game");
+        //RETRIEVING THE JOIN MESSAGE ALREADY SET
+        String joinMessage = evt.getJoinMessage();
 
-            //CLEARING THE JOIN MESSAGE
-            evt.setJoinMessage(null);
-            }
-        }
+        //SETTING THE JOIN MESSAGE
+        evt.setJoinMessage(joinedName + " has joined the game");
+
+        //CLEARING THE JOIN MESSAGE
+        evt.setJoinMessage(null);
+    }
+}
