3f0d73bb7805de3fb41b0eccc7bf00391bda4ffa
wuize <102895127+wuize@users.noreply.github.com>
2022-09-28 19:36:26 +0800

    Update diamond.java

--- src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/diamond.java
+++ src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/diamond.java
@@ -25,16 +25,16 @@ public class diamond implements CommandExecutor {
                 if (!sender.isOp()) {
 
                     sender.sendMessage("§b[鑽石] §d==========教學==========");
-                    sender.sendMessage("§b/diamond help     §d |    教學清單 ✔ ");
-                    sender.sendMessage("§b/diamond say       §d|    每個人都可以輸入 ✔");
-                    sender.sendMessage("§b/diamond text      §d|    每個人都可以輸入 ✔");
+                    sender.sendMessage("§b/dwhelp     §d |    教學清單 ✔ ");
+                    sender.sendMessage("§b/dwsay       §d|    每個人都可以輸入 ✔");
+                    sender.sendMessage("§b/dwtext      §d|    每個人都可以輸入 ✔");
                 } else {
                     sender.sendMessage("§b[鑽石] §d=========管理員教學=========");
-                    sender.sendMessage("§b/diamond help     §d |    教學清單 ✔ ");
-                    sender.sendMessage("§b/diamond say       §d|    每個人都可以輸入 ✔");
-                    sender.sendMessage("§b/diamond text      §d|    每個人都可以輸入 ✔");
-                    sender.sendMessage("§b/diamond give      §d|    給予鑽石 要有op ✔");
-                    
+                    sender.sendMessage("§b/dwhelp     §d |    教學清單 ✔ ");
+                    sender.sendMessage("§b/dwsay       §d|    每個人都可以輸入 ✔");
+                    sender.sendMessage("§b/dwtext      §d|    每個人都可以輸入 ✔");
+                    sender.sendMessage("§b/dwgive      §d|    給予鑽石 要有op ✔");
+
 
                     return true;
                 }
@@ -54,7 +54,7 @@ public class diamond implements CommandExecutor {
                 if (!sender.isOp()) {
                     sender.sendMessage("§b[鑽石] §a你沒有權限執行這個指令");
                 } else if (args.length != 2) {
-                    sender.sendMessage("§b[鑽石] §a用法：/diamond server <訊息>");
+                    sender.sendMessage("§b[鑽石] §a用法：/dwserver <訊息>");
                 } else {
                     String Message = null;
                     for (int i = 2; i <= args.length; i++) {
@@ -77,39 +77,13 @@ public class diamond implements CommandExecutor {
             }
             if (args[0].equals("help")) {
                 sender.sendMessage("§b[鑽石大守衛] §d==========教學==========");
-                sender.sendMessage("§b/diamond help    §d  |    教學清單 ✔ ");
-                sender.sendMessage("§b/diamond say    §d   |    每個人都可以輸入 ✔");
-                sender.sendMessage("§b/diamond text     §d |    每個人都可以輸入 ✔");
-                sender.sendMessage("§b/diamond give     §d |    有op的人輸入會有鑽石 ✔ ");
+                sender.sendMessage("§b/dwhelp    §d  |    教學清單 ✔ ");
+                sender.sendMessage("§b/dwsay    §d   |    每個人都可以輸入 ✔");
+                sender.sendMessage("§b/dwtext     §d |    每個人都可以輸入 ✔");
+                sender.sendMessage("§b/dwgive     §d |    有op的人輸入會有鑽石 ✔ ");
                 return true;
 
             }
-
-            if (args[0].equals("giveop")) {
-                ItemStack 回血書 = new ItemStack(Material.COMMAND_BLOCK, 1);
-                ItemMeta meta = 回血書.getItemMeta();
-                meta.setDisplayName("§c回血書");
-                ArrayList lore = new ArrayList();
-                lore.add("§b手持回血, 每秒回血1滴血");
-                meta.setLore(lore);
-                meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                回血書.setItemMeta(meta);
-
-                ItemStack 升級寶石 = new ItemStack(Material.COMMAND_BLOCK, 1);
-                ItemMeta meta2 = 升級寶石.getItemMeta();
-                meta2.setDisplayName("§a升級寶石");
-                ArrayList lore2 = new ArrayList();
-                lore2.add("§b手持該物品持續加經驗, 每0.5秒加1經驗");
-                meta2.setLore(lore2);
-                meta2.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                升級寶石.setItemMeta(meta2);
-
-                sender.getServer().getPlayer(sender.getName()).getInventory().addItem(new ItemStack(回血書));
-                sender.getServer().getPlayer(sender.getName()).getInventory().addItem(new ItemStack(升級寶石));
-            }
-
-            return false;
-
         }
         return false;
     }
