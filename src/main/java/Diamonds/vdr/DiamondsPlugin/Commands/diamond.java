d8cbb154521ce034bae7f441c24f7d86225f8c3e
allens987 <97739580+allens987@users.noreply.github.com>
2023-01-01 08:14:47 +0800

    Update diamond.java

--- src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/diamond.java
+++ src/main/java/Diamonds/vdr/DiamondsPlugin/Commands/diamond.java
@@ -1,95 +1,41 @@
-package Diamonds.vdr.DiamondsPlugin.Commands;
-
-import java.util.ArrayList;
-
+// Import necessary classes
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemFlag;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.scheduler.BukkitRunnable;
 
-public class diamond implements CommandExecutor {
+// Create a new class for the custom menu
+public class CustomMenu implements CommandExecutor, Listener {
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+  // Create a new inventory for the menu
+  private Inventory menu = Bukkit.createInventory(null, 9, "Custom Menu");
 
-        if (args.length == 0) {
-            if (sender instanceof Player) {
-                if (!sender.isOp()) {
-
-                    sender.sendMessage("§b[鑽石] §d==========教學==========");
-                    sender.sendMessage("§b/diamond help     §d |    教學清單 ✔ ");
-                    sender.sendMessage("§b/diamond say       §d|    每個人都可以輸入 ✔");
-                    sender.sendMessage("§b/diamond text      §d|    每個人都可以輸入 ✔");
-                } else {
-                    sender.sendMessage("§b[鑽石] §d=========管理員教學=========");
-                    sender.sendMessage("§b/diamond help     §d |    教學清單 ✔ ");
-                    sender.sendMessage("§b/diamond say       §d|    每個人都可以輸入 ✔");
-                    sender.sendMessage("§b/diamond text      §d|    每個人都可以輸入 ✔");
-                    sender.sendMessage("§b/diamond give      §d|    給予鑽石 要有op ✔");
-
-
-                    return true;
-                }
-
-            }
-
-            if (args[0].equals("say")) {
-                sender.sendMessage("§b[鑽石]§a hi 你好你有任何事情可以輸入 help");
-                return true;
-            }
-            if (args[0].equals("text")) {
-                sender.sendMessage("§b[鑽石] §a你是在哭喔你text幹嘛啦");
-                return true;
-            
-            }
-            if (args[0].equals("lol")) {
-                        Bukkit.getServer().broadcastMessage("§5[YeYanChen_9487tw] :§a 大家好我是無腦屁孩 我超智障 ");
-                return true;
-            
-            }
-            if (args[0].equals("server")) {
-                if (!sender.isOp()) {
-                    sender.sendMessage("§b[鑽石] §a你沒有權限執行這個指令");
-                } else if (args.length != 2) {
-                    sender.sendMessage("§b[鑽石] §a用法：/diamond server <訊息>");
-                } else {
-                    String Message = null;
-                    for (int i = 2; i <= args.length; i++) {
-                        Message = Message + args[i - 1];
-                        Bukkit.getServer().broadcastMessage("§b[鑽石大公告] §e  " + Message);
-                    }
-                }
-            }
-            if (args[0].equals("give")) {
-                if (sender instanceof Player) {
-                    if (!sender.isOp()) {
-
-                        sender.sendMessage("§b[鑽石大守衛]§a 此指令只有玩家才能執行");
-                        sender.sendMessage("§b[鑽石大守衛] §a你沒有權限執行這個指令");
-                    } else {
-                        sender.getServer().getPlayer(sender.getName()).getInventory().addItem(new ItemStack(Material.DIAMOND, 64));
-                        return true;
-                    }
-                }
-            }
-            if (args[0].equals("help")) {
-                sender.sendMessage("§b[鑽石大守衛] §d==========教學==========");
-                sender.sendMessage("§b/diamond help    §d  |    教學清單 ✔ ");
-                sender.sendMessage("§b/diamond say    §d   |    每個人都可以輸入 ✔");
-                sender.sendMessage("§b/diamond text     §d |    每個人都可以輸入 ✔");
-                sender.sendMessage("§b/diamond give     §d |    有op的人輸入會有鑽石 ✔ ");
-                return true;
-
-            }
-        }
-        return false;
+  // Handle the /menu command to open the custom menu
+  @Override
+  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+    // Check if the sender is a player
+    if (sender instanceof Player) {
+      // Open the custom menu for the player
+      Player player = (Player) sender;
+      player.openInventory(menu);
+      return true;
     }
+    return false;
+  }
+
+  // Handle player interaction with the menu
+  @EventHandler
+  public void onInventoryClick(InventoryClickEvent event) {
+    // Check if the inventory is the custom menu
+    if (event.getInventory().equals(menu)) {
+      // Handle the player's action here
+      event.setCancelled(true);
+    }
+  }
 }
